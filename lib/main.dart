55bd838234576b966d950ee4e5a442aedd016068
ysimsek~ <ysimsek631@gmail.com>
2025-11-27 09:34:09 +0300

    gh

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // furkan can seni çok seviyorun
+
 
   final String title;
 
